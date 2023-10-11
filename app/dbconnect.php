cd961cef4a1fa26266ee54f0722a50d5cdf5e489
pm <ampm65535@gmail.com>
2023-10-11 16:15:19 +0900

    delete dbconnect for network

--- app/dbconnect.php
+++ app/dbconnect.php
@@ -5,15 +5,9 @@ $dbname = 'mydatabase';
 $username = 'user';
 $password = 'password';
 
-// try {
-//     $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-// } catch(PDOException $e) {
-//     echo "Connection failed: " . $e->getMessage();
-// }
-
 try {
     $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 } catch(PDOException $e) {
-    //$db_error = "Connection failed: " . $e->getMessage(); //エラーメッセージを出力せずに変数に格納する
+    $db_error = "Connection failed: " . $e->getMessage(); //エラーメッセージを出力せずに変数に格納する
 }
 ?>
