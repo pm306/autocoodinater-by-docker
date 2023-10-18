0f4e0e8d34cb07cde9bf3a47631202fea62ce1ba
pm <ampm65535@gmail.com>
2023-10-18 15:41:11 +0900

    refactor: clothes_details
    ファイル名変更
    リファクタリング

---
+++ app/clothes_details.php
@@ -0,0 +1,33 @@
+<?php 
+session_start();
+require_once('logincheck.php');
+require_once('header.php');
+require_once('dbconnect.php');
+require_once('clothes_type.php');
+require_once('utils.php'); 
+
+$clotheDetails = array();
+
+if(isset($_POST[POST_PICTURE_ID_KEY])){
+    $clotheDetails = fetchClotheDetails($_POST[POST_PICTURE_ID_KEY]);
+}
+
+$chothes_type = $clothes_type_tops[$clotheDetails['type']] ?? $clothes_type_bottoms[$clotheDetails['type']] ?? "";
+$escaped_clothe_id = htmlspecialchars($clotheDetails['id'], ENT_QUOTES, 'UTF-8');
+$escaped_clothe_picture = htmlspecialchars($clotheDetails['picture'], ENT_QUOTES, 'UTF-8');
+
+?>
+<p>種別：<?php echo $clothetype; ?>
+<form method="post" name="deleteForm" action="delete_clothe.php">
+    <input type="hidden" name="id" value="<?php echo $escaped_clothe_id ?>">
+    <a href="javascript:deleteForm.submit()">データの削除(クリックすると消去されます)</a>
+</form>
+
+<hr>
+<img id="detail" src="upload/<?php echo $escaped_clothe_picture?>">
+<form action="closet.php" name="returnForm" method="post">
+    <input type="hidden" name="return" value="true">
+    <a class="return" href="javascript:returnForm.submit()"><img src="pictures/navigationj_back.png" width="100" height="50"></a>
+</form>
+
+<?php require_once('footer.php')?>
--- app/templates/imageFormTemplate.php
+++ app/templates/imageFormTemplate.php
@@ -1,4 +1,4 @@
-<form method="post" name="<?= $formName ?>" action="clothe_details.php" style="display:inline">
+<form method="post" name="<?= $formName ?>" action="clothes_details.php" style="display:inline">
     <input type="hidden" name="picture_id" value="<?= $pictureId ?>">
     <a href="javascript:document.<?= $formName ?>.submit()" style='text-decoration: none;'>
         <img id="result" src="<?= $imageSrc ?>" width="200" height="200">
